8118f6497ad47e8e534f35f728da26b185956a06
Eli Murphy <emurphy24@gcds.net>
2021-10-07 10:12:54 -0400

    Update StringsAndThings.py

--- StringsAndThings.py
+++ StringsAndThings.py
@@ -7,6 +7,7 @@ Documentation
 def main():
     '''
     User puts in the word; word=string
+    word = user input that is being altered
     User puts in how many characters they want to be altered
     total = how many total characters are in the string 
     numberChars = amount of characters that will be altered 
@@ -14,46 +15,50 @@ def main():
     direction = right or left, what direction are the "numberChars" amount of characters moved
     '''
 
-def ls(numberChars):
+def ls(numberChars, word):
     '''
-    calls string and variable "numberChars" from main
+    calls word and variable "numberChars" from main
     Deletes the "numberChars" amt of characters starting with the first character (leftmost)
     Inputs "numberChars" number of # at the end of the string
+    start = before first character
     return the string with "numberChars" amt of characters deleted, starting from the first character (leftmost) AND "numberChars" amt of # after the last character (rightmost)
     '''
     
-def rs(numberChars):
+def rs(numberChars, word):
     '''
-    calls string and variable "numberChars" from main
+    calls word and variable "numberChars" from main
     Deletes the "numberChars" amt of characters starting with the last character (rightmost)
     Inputs "numberChars" number of # at the beggining of the string
+    start = after last character
     return the string with "numberChars" amt of characters deleted, starting from the last character (rightmost) AND "numberChars" amt of # before the first character (leftmost)
     '''
-def lc(numberChars):
+def lc(numberChars, word):
     '''
-    calls string and variable "numberChars" from the main
+    calls word and variable "numberChars" from the main
     takes "numberChars" amt of characters starting with the first character (leftmost) and moves "numberChars" amt of characters to after the last character (rightmost)
+    start = before first character
     return the string with the first "numberChars" amt of characters moved to after the last (rightmost) character
     '''
     
-def rc(numberChars): 
+def rc(numberChars, word): 
     '''
-    calls string and variable "numberChars" from the main
+    calls word and variable "numberChars" from the main
     takes "numberChars" amt of characters starting with the last character (rightmost)and moves "numberChars" amt of characters to before the first character (leftmost)
+    start = after last character
     return the string with the last "numberChars" amt of characters moved to before the first (leftmost) character
     '''
-def mc(start,total,numberChars,direction):
+def mc(start,total,numberChars,direction, word):
     '''
-    calls string and variable "numberChars", "start", "total", "direction" from the main
-    starting with position start (instead of leftmost and rightmost it is start position)
+    calls word and variable "numberChars", "start", "total", "direction" from the main
+    start = before position inputted (instead of leftmost and rightmost it is start position)
     start at position "start" out of "total" and move "numberChars" amt of characters in the direction "direction"
     return the string following the above arguements
     '''
     
-def rev(start,total):
+def rev(start, total, word):
     '''
-    reverse order of string 
-    call "start" from main, start with position "start" out of "total" 
+    reverse order of word 
+    call word from main, start with position "start" out of "total" 
     return the string starting with the last character (rightmost) and ending with the first (leftmost)
     '''
 
