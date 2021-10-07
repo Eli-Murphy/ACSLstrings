dbca840440f61b27169a02c828bb407848ad1818
Eli Murphy <emurphy24@gcds.net>
2021-10-07 10:19:06 -0400

    Update eliworkspace.py

---
+++ eliworkspace.py
@@ -0,0 +1,12 @@
+def main():
+    start=
+    total=
+    numberChars=
+    word=
+    mc(start,total,numberChars,direction, word)
+    
+    
+def mc(start,total,numberChars,direction, word):
+    
+if __name__ == '__main__':
+        main()
