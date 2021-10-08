aea7edcda88e2d3626b7e6360a25d3abfd08b697
Eli Murphy <emurphy24@gcds.net>
2021-10-08 11:27:44 -0400

    Update eliworkspace.py

--- eliworkspace.py
+++ eliworkspace.py
@@ -4,12 +4,26 @@ def main():
     numberChars=2
     direction="R"
     word= "COMPUTER"
-    mc(start,total,numberChars,direction, word)
+    #mc(start,total,numberChars,direction, word)
+    rev(start,total,word)
     
     
 def mc(start,total,numberChars,direction, word):
+    wordlist = list(word)
+    start = start - 1
+    print(wordlist)
     
+def rev(start, total,word):
+    start = start -2
+    end = start+total
+    wordlist = list(word)
+    i = slice(end,start,-1)
+    print(wordlist[i])
+    del wordlist[start+1:end+1]
+    print(wordlist)
+    endlist = []
+    endlist[start:end] = 
     
     
 if __name__ == '__main__':
-        main()
+    main()
