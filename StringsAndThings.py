052f5400e0ea9e85ce53ddd3f68a33c8c2a50fa9
loscher <79922035+loscher@users.noreply.github.com>
2021-10-11 17:40:23 -0400

    Update StringsAndThings.py

--- StringsAndThings.py
+++ StringsAndThings.py
@@ -14,7 +14,8 @@ def main():
     start = starting position
     direction = right or left, what direction are the "numberChars" amount of characters moved
     '''
-    try:   
+    
+try:   
     word_string = (input('Enter word you would like altered'))
     if word_string.isalpha() == False:
         raise ValueError()
