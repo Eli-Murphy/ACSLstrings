8620ead4ced96c660b3d4ce603a9777968490fd2
Eli Murphy <emurphy24@gcds.net>
2021-10-12 21:03:29 -0400

    Update eliworkspace.py

--- eliworkspace.py
+++ eliworkspace.py
@@ -9,20 +9,26 @@ def main():
     
     
 def mc(start,total,numberChars,direction, word):
-    wordlist = list(word)
+    wordList = list(word)
     start = start - 1
-    print(wordlist)
+    print(wordList)
     
 def rev(start, total,word):
-    start = start -2
-    end = start+total
-    wordlist = list(word)
-    i = slice(end,start,-1)
-    print(wordlist[i])
-    del wordlist[start+1:end+1]
-    print(wordlist)
-    endlist = []
-    endlist[start:end] = 
+    start = start -2                                                                 #I have no idea why this works it just does
+    end = start+total                                                             #This calculates the end point by adding the total amount of chars effected
+    wordList = list(word)                                                         #Converts desired word to list
+    i = slice(end,start,-1)                                                         #Creates index var that splits wordList into desired section and reverses the section
+    revChar = wordList[i]                                                         #Assigns split to variable
+    print(revChar)
+    del wordList[start+1:end+1]                                                #deletes section from original function
+    print(wordList)
+    wordList.insert(start+1, revChar)                                          #This puts the reversed segment list into the wordList list
+    print(wordList)
+    flatList = [item for sublist in wordList for item in sublist]        #This flattens the list inside the list into one list
+    print(flatList)                     
+    output = ''.join(flatList)                                                          #This converts the list into a string
+    print("Output: " + output)
+    
     
     
 if __name__ == '__main__':
