3bcd951220b51d884a12a039e1dc32e86753557c
Eli Murphy <emurphy24@gcds.net>
2021-10-14 09:34:31 -0400

    Create eliworkspace.py

--- eliworkspace.py
+++ eliworkspace.py
@@ -1,5 +1,6 @@
 def main():
-    start=3
+    start=0
+
     total=3
     numberChars=2
     direction="R"
@@ -13,14 +14,21 @@ def mc(start,total,numberChars,direction, word):
     start = start - 1
     print(wordList)
     
-def rev(start, total,word):
-    start = start -2                                                                 #I have no idea why this works it just does
+def rev(start, total,word): 
+    if start <= 0:
+        return "Error: Starting position must be greater then 0."
+        main()
+    start = start -1                                                            #Dealing with offset by subtracting 1
+    total = total - 1                                                                   #Dealing with offset by subtracting 1
     end = start+total                                                             #This calculates the end point by adding the total amount of chars effected
     wordList = list(word)                                                         #Converts desired word to list
-    i = slice(end,start,-1)                                                         #Creates index var that splits wordList into desired section and reverses the section
-    revChar = wordList[i]                                                         #Assigns split to variable
-    del wordList[start+1:end+1]                                                #deletes section from original function
-    wordList.insert(start+1, revChar)                                          #This puts the reversed segment list into the wordList list
+    if start > 0:
+        revChar = wordList[end:start-1:-1]
+    else:
+        revChar = wordList[:end+1]
+        revChar = revChar[::-1]
+    del wordList[start:end+1]                                                #deletes section from original function
+    wordList.insert(start, revChar)                                          #This puts the reversed segment list into the wordList list
     flatList = [item for sublist in wordList for item in sublist]        #This flattens the list inside the list into one list            
     output = ''.join(flatList)                                                          #This converts the list into a string
     print("Output: " + output)
