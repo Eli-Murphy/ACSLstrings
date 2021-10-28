c7de49071c8da154bdaee1631633fbd4dde60aa3
Eli Murphy <emurphy24@gcds.net>
2021-10-28 10:15:01 -0400

    Update eliworkspace.py

--- eliworkspace.py
+++ eliworkspace.py
@@ -4,31 +4,74 @@ def main():
     total=3 
     numberChars=2
     direction="L"
-    word= "logan"
-    mc(start,total,numberChars,direction, word)
+    word= "COMPUTER"
+    print(mc(start,total,numberChars,direction, word))
     #rev(start,total,word)
     #mc(start,total,numberChars,direction,word)
     
     
+# def mc(start,total,numberChars,direction, word):
+#     start = start -1
+#     def rightrotate(subString, numberChars):
+#         return leftrotate(subString, len(subString) - numberChars)
+#     def leftrotate(subString,numberChars):
+#         tmp = subString[numberChars : ] + subString[0 : numberChars]
+#         return tmp
+#         
+#     if direction == "L":
+#         total = total + start
+#         subString = word[start:total]
+#         print(word[:start] + leftrotate(subString,numberChars) + word[total:])
+#         return (word[:start] + leftrotate(subString,numberChars) + word[total:])
+#     
+#     if direction == "R":
+#         total = total + start
+#         subString = word[start:total]
+#         print(word[:start] + rightrotate(subString, numberChars) + word[total:])
+#         return (word[:start] + rightrotate(subString, numberChars) + word[total:])
+#  
+#     
+    
 def mc(start,total,numberChars,direction, word):
+    '''
+    calls word and variable 'numberChars', 'start', 'total', 'direction' from the main
+    start = before position inputted (instead of leftmost and rightmost it is start position)
+    start at position 'start' out of 'total' and move 'numberChars' amt of characters in the direction 'direction'
+    return the string following the above arguements
+    '''
     start = start -1
-    def rightrotate(subString, numberChars):
-        return leftrotate(subString, len(subString) - numberChars)
-    def leftrotate(subString,numberChars):
+    def RR(subString, numberChars):
+        return LR(subString, len(subString) - numberChars)
+    def LR(subString,numberChars):
         tmp = subString[numberChars : ] + subString[0 : numberChars]
         return tmp
         
-    if direction == "L":
+    if direction == 'L' or direction == 'l':
         total = total + start
         subString = word[start:total]
-        print(word[:start] + leftrotate(subString,numberChars) + word[total:])
-        return (word[:start] + leftrotate(subString,numberChars) + word[total:])
+        
+        firstseg = word[:start]
+        modedseg = LR(subString,numberChars)
+        lastseg = word[total:]
+        
+        return (firstseg+modedseg+lastseg)
+        print (firstseg+modedseg+lastseg)
+        #print('mc version:' , word[:start] + leftrotate(subString,numberChars) + word[total:])
+        #return (word[:start] + leftrotate(subString,numberChars) + word[total:])
     
-    if direction == "R":
+    if direction == 'R' or direction == 'r':
         total = total + start
         subString = word[start:total]
-        print(word[:start] + rightrotate(subString, numberChars) + word[total:])
-        return (word[:start] + rightrotate(subString, numberChars) + word[total:])
+        
+        firstseg = word[:start]
+        modedseg = RR(subString,numberChars)
+        lastseg = word[total:]
+        
+        return (firstseg+modedseg+lastseg)
+        print (firstseg+modedseg+lastseg)
+        
+        #print('mc version:' , word[:start] + rightrotate(subString, numberChars) + word[total:])
+        #return (word[:start] + rightrotate(subString, numberChars) + word[total:])
  
     
     
